20d3ef155061f81b5011612f8632cbee291fd9a2
chojs23 <chojs990222@gmail.com>
2020-09-11 18:46:21 +0900

    initialize

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAfIjQOVPxSc4dJl2ruC07ksYz6EuG4e7g',
+  authDomain: 'whatsapp-clone-e4b37.firebaseapp.com',
+  databaseURL: 'https://whatsapp-clone-e4b37.firebaseio.com',
+  projectId: 'whatsapp-clone-e4b37',
+  storageBucket: 'whatsapp-clone-e4b37.appspot.com',
+  messagingSenderId: '1089031789295',
+  appId: '1:1089031789295:web:b1fa791b619d76ca60d91a',
+  measurementId: 'G-7L25LR98EZ',
+};
